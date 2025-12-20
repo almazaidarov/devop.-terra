f8cc5c5a4fa2d5f64a3d039e4fd8163eac088d1b
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-20 01:52:13 -0600

    added info

--- class1/main.tf
+++ class1/main.tf
@@ -1,3 +1,8 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "terraform_class"
+    managed_by = "https://github.com/almazaidarov/devop.-terra"
+  }
 }
