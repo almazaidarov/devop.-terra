8b365ee3ebeef7dbee3c41de86de4c67622b2ad5
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-22 13:50:51 -0600

    added public ip

--- change2/output.tf
+++ change2/output.tf
@@ -7,3 +7,8 @@ output arn {
      description = "The arn of the web instance"
      value =  aws_instance.web.id
 }
+
+output public_ip {
+     description = "The public IP of the web instance"
+     value =  aws_instance.web.public_ip
+}
