404a3b1037d53fcf1c377a7c92ddf0211b61f3f1
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-22 19:35:43 -0600

    fixed

--- change2/main.tf
+++ change2/main.tf
@@ -9,12 +9,12 @@
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+     name   = "virtualization-type"
+     values = ["hvm"]
+  }
 
-#   owners = ["099720109477"] # Canonical
+   owners = ["099720109477"] # Canonical
 
 
 
