5848fc43ad468f7ba54f29d314c97d547ea2b1e0
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-22 19:21:39 -0600

    added az

--- change2/main.tf
+++ change2/main.tf
@@ -1,4 +1,4 @@
- data "aws_ami" "ubuntu" {
+ #data "aws_ami" "ubuntu" {
 #   most_recent = true
 
 #   filter {
@@ -12,7 +12,7 @@
 #   }
 
 #   owners = ["099720109477"] # Canonical
-}
+#}
 
 
 # resource "aws_security_group" "wordpress-terraform" {
@@ -54,16 +54,17 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
+  
   enable_nat_gateway = false
   enable_vpn_gateway = false
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+
 }
 
+
+output private_subnets{
+  value = module.vpc.private_subnets
+}
 
 
 module "asg" {
@@ -75,9 +76,8 @@ module "asg" {
   desired_capacity          = 3
   wait_for_capacity_timeout = 0
   health_check_type         = "EC2"
-  vpc_zone_identifier       = [
-    module.vpc.private_subnets
-  ]
+  availability_zones        = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  
 
 
   # Launch template
