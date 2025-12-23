4f60b0152fece90172d78a770eb7bb78ff91f0ac
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-22 19:36:44 -0600

    added ubuntu ownres

--- change2/main.tf
+++ change2/main.tf
@@ -1,20 +1,20 @@
  data "aws_ami" "ubuntu" {
- most_recent = true
- }
+  most_recent = true
 
-
-
- filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
- }
+  owners = ["099720109477"]  # Canonical (official Ubuntu)
 
   filter {
-     name   = "virtualization-type"
-     values = ["hvm"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
-   owners = ["099720109477"] # Canonical
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
 
 
 
