7cfefa2cbe547bc5f6ff5430cf5359aaf6f757a4
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-22 18:56:41 -0600

    asg created

--- change2/main.tf
+++ change2/main.tf
@@ -63,3 +63,30 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+
+
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  # Autoscaling group
+  name = "example-asg"
+  min_size                  = 1
+  max_size                  = 3
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = [
+    module.vpc.private_subnets
+  ]
+
+
+  # Launch template
+  launch_template_name        = "example-asg"
+  launch_template_description = "Launch template example"
+  update_default_version      = true
+
+  image_id          = "data.aws_ami.ubuntu.id"
+  instance_type     = "t3.micro"
+  ebs_optimized     = false
+  enable_monitoring = false
+}
