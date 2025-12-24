6ed6663f1d55cb7ae8d52328d78ec71011f13394
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-23 21:14:48 -0600

    main.tf chamge

--- change2/main.tf
+++ change2/main.tf
@@ -78,15 +78,75 @@ module "asg" {
   health_check_type         = "EC2"
   availability_zones        = ["us-east-1a", "us-east-1b", "us-east-1c"]
   
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
 
+  name    = "my-alb"
+  vpc_id  = "vpc-abcde012"
+  subnets = ["subnet-abcde012", "subnet-bcde012a"]
 
-  # Launch template
-  launch_template_name        = "example-asg"
-  launch_template_description = "Launch template example"
-  update_default_version      = true
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
 
-  image_id          = data.aws_ami.ubuntu.id
-  instance_type     = "t3.micro"
-  ebs_optimized     = false
-  enable_monitoring = false
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
+
 }
