218b1a0ee40e69ad279e073147f89fa899c6817f
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-01 21:14:37 -0800

    added subnets

--- change3/main.tf
+++ change3/main.tf
@@ -11,3 +11,30 @@ resource "azurerm_virtual_network" "change3-network" {
   location            = azurerm_resource_group.change3.location
   address_space       = ["10.0.0.0/16"]
 }
+
+
+resource "azurerm_subnet" "change3-subnet1" {
+  name                 = "change3-subnet1"
+  resource_group_name  = azurerm_resource_group.change3.name
+  virtual_network_name = azurerm_virtual_network.change3.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+
+resource "azurerm_subnet" "change3-subnet2" {
+  name                 = "change3-subnet2"
+  resource_group_name  = azurerm_resource_group.change3.name
+  virtual_network_name = azurerm_virtual_network.change3.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+}
+
+
+resource "azurerm_subnet" "change3-subnet3" {
+  name                 = "change3-subnet3"
+  resource_group_name  = azurerm_resource_group.change3.name
+  virtual_network_name = azurerm_virtual_network.change3.name
+  address_prefixes     = ["10.0.3.0/24"]
+
+}
