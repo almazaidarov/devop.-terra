548a51002e9095e7b4b0274f6af585b3930788d5
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-01 21:39:02 -0800

    fixed

--- change3/main.tf
+++ change3/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "change3_network" {
 resource "azurerm_subnet" "change3_subnet1" {
   name                 = "change3_subnet1"
   resource_group_name  = azurerm_resource_group.change3.name
-  virtual_network_name = azurerm_virtual_network.change3.name
+  virtual_network_name = azurerm_virtual_network.change3_network.name
   address_prefixes     = ["10.0.1.0/24"]
 
 }
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "change3_subnet1" {
 resource "azurerm_subnet" "change3_subnet2" {
   name                 = "change3_subnet2"
   resource_group_name  = azurerm_resource_group.change3.name
-  virtual_network_name = azurerm_virtual_network.change3.name
+  virtual_network_name = azurerm_virtual_network.change3_network.name
   address_prefixes     = ["10.0.2.0/24"]
 
 }
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "change3_subnet2" {
 resource "azurerm_subnet" "change3_subnet3" {
   name                 = "change3_subnet3"
   resource_group_name  = azurerm_resource_group.change3.name
-  virtual_network_name = azurerm_virtual_network.change3.name
+  virtual_network_name = azurerm_virtual_network.change3_network.name
   address_prefixes     = ["10.0.3.0/24"]
 
 }
