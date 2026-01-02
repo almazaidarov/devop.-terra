7157b57c332082c5fa9f37821dd35cba302a3529
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-01 21:06:47 -0800

    added vnet (vpc)

--- change3/main.tf
+++ change3/main.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "change3" {
   name     = "change3-resources"
   location = "West Europe"
 }
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "change3-network" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.change3.name
+  location            = azurerm_resource_group.change3.location
+  address_space       = ["10.0.0.0/16"]
+}
