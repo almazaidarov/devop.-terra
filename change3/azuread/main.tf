44b3434e414a2dd47d341c4a7eaba8a13e3262f6
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-01 22:56:11 -0800

    added usergroup

--- change3/azuread/main.tf
+++ change3/azuread/main.tf
@@ -4,22 +4,6 @@ resource "random_password" "password" {
   override_special = "_%@"
 }
 
-resource "azuread_user" "example" {
-  user_principal_name = "jdoe@${data.azuread_client_config.current.primary_domain}"
-  display_name        = "J. Doe"
-  given_name         = "J."
-  surname            = "Doe"
-  mail_nickname       = "jdoe"
-  password            = "Secret1234!"
-    force_password_change = false
-  company_name       = "Aidaroff Inc"
-  country           = "US"
-  city              = "Seattle" 
-  postal_code     = "98101"
-  state            = "WA"   
-
-}
-
 
 
 resource "azuread_group" "example" {
