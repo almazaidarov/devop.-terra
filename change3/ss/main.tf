28ddc49acf1e14820b42107183ab3126654cc154
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-05 12:49:29 -0800

    changed to n_europr

--- change3/ss/main.tf
+++ change3/ss/main.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "North Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
