05c5f5c99b63523636028c2f0454f5b6c38d8113
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-05 12:42:40 -0800

    fixed

--- change3/ss/main.tf
+++ change3/ss/main.tf
@@ -1,5 +1,6 @@
 locals {
   first_public_key = "SHA256:VUC6jVN15A0RbckbKWiMChsvBDXGaP9GOHcEayaDDIg almaz@SandboxHost-639032277261145227"
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
