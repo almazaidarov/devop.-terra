0b0f14c4e290388e6917711cfa552c06e2013026
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-05 12:32:40 -0800

    fixed public_key

--- change3/ss/main.tf
+++ change3/ss/main.tf
@@ -31,7 +31,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("/home/almaz/~/.ssh/id_rsa.pub")
+    public_key = file("/home/almaz/.ssh/id_rsa.pub")
+
   }
 
   source_image_reference {
