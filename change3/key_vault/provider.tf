e701ffece589d2569e135525909c92c9e01d9ed7
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-06 11:28:26 -0800

    added key_secret

--- change3/key_vault/provider.tf
+++ change3/key_vault/provider.tf
@@ -2,14 +2,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "~> 3.100"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = "5f693d88-2d46-4814-b35e-672e4a203a71"
-}
 
+provider "azurerm" {
+  subscription_id = "5f693d88-2d46-4814-b35e-672e4a203a71"
+
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
