740e2d1b16ae200bb4f85c101c3b03bce3190414
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-06 11:38:08 -0800

    added permission

--- change3/key_vault/main.tf
+++ change3/key_vault/main.tf
@@ -20,17 +20,13 @@ resource "azurerm_key_vault" "example" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions = [
-      "Get",
-    ]
-
+    
     secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+  ]
   }
 }
 
