9785ab187d08835df22214839ae0be264befda13
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-07 11:20:35 -0800

    fixed project _d

--- change4_GCP/account/main.tf
+++ change4_GCP/account/main.tf
@@ -11,7 +11,7 @@ resource "random_password" "password" {
 
 resource "google_project" "my_project" {
   name       = "My Terraform Project"
-  project_id = "tf-${random_id.project.hex}"
+  project_id = my-terraform-project-123-almaz"
   billing_account = data.google_billing_account.acct.id
 }
 
