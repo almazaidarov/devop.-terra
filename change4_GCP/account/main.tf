7e886d5342e13228b07380a312b1b8a0c2408b72
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2026-01-07 11:26:58 -0800

    fixed main.tf

--- change4_GCP/account/main.tf
+++ change4_GCP/account/main.tf
@@ -3,31 +3,21 @@ data "google_billing_account" "acct" {
   open         = true
 }
 
-resource "random_password" "password" {
-  length = 16
-  special = true
-  override_special = "_%@"
-}
 
 resource "google_project" "my_project" {
-  name       = "My Terraform Project"
-  project_id = my-terraform-project-123-almaz"
-  billing_account = data.google_billing_account.acct.id
+  name             = "My Terraform Project"
+  project_id       = "my-terraform-project-123-almaz"
+  billing_account  = data.google_billing_account.acct.id
 }
 
+resource "null_resource" "set_project" {
+  depends_on = [google_project.my_project]
 
- resource "null_resource" "set-project" {
-   triggers = {
-     always_run = timestamp()
-   }
- }
+  triggers = {
+    always_run = timestamp()
+  }
 
- 
- 
- resource "null_resource" "set_project" {
-   depends_on = [google_project.my_project]
- 
-   provisioner "local-exec" {
-     command = "gcloud config unset project && gcloud config set project ${google_project.my_project.project_id}"
-   }
- }
+  provisioner "local-exec" {
+    command = "gcloud config unset project && gcloud config set project ${google_project.my_project.project_id}"
+  }
+}
